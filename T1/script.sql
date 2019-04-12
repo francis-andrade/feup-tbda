d4cf1ef07b71451fa06da11d0fc930821d72e0e7
francis-andrade <up201503481@fe.up.pt>
2019-04-12 09:17:07 +0100

    removed irrelevant files

--- T1/script.sql
+++
@@ -1,4 +0,0 @@
-SET TIMING OFF;
-SELECT a.numero AS "Aluno (BI)", a.a_lect_conclusao - a.a_lect_matricula AS "Anos Demorados" 
-FROM zalus a JOIN zlics l  ON a.curso = l.codigo 
-WHERE a.estado = 'C' AND a.a_lect_conclusao - a.a_lect_matricula < 5 AND l.sigla = 'EIC';
