bac354ee0063044a05efc64b3b582d72b5c5711e
cyrilico <jpdamas15@gmail.com>
2019-05-19 18:31:02 +0100

    Migration script adjustments

--- T3/migrate.py
+++ T3/migrate.py
@@ -1,6 +1,6 @@
 import xmltodict
 import json
-from pymongo import MongoClient
+import pymongo
 
 # Parses key to lowercase strings (more conventional for mongo) and parses int values as int (when applicable)
 def postprocessor(path, key, value):
@@ -11,7 +11,7 @@ def postprocessor(path, key, value):
 
 # Parser when seeing municipalities with one facility parses it as object and not array of object. Same goes for facilities with one activity. This fixes that
 def parse_one_item_arrays(m_dict):
-    for d in m_dict['data']['district']:
+    for d in m_dict['data']['districts']:
         for m in d['municipalities']:
             if 'facilities' in m:
                 if type(m['facilities']) is not list:
@@ -24,11 +24,14 @@ def parse_one_item_arrays(m_dict):
 
 MY_XML_HERE = """..."""
 
-my_dict = xmltodict.parse(MY_XML_HERE, postprocessor=postprocessor, encoding='ISO-8859-1')
+my_dict = xmltodict.parse(MY_XML_HERE, postprocessor=postprocessor)
 
-client = MongoClient("mongodb://tbda:grupoa@vdbase.inesctec.pt:27017/tbda?authSource=admin")
+client = pymongo.MongoClient("mongodb://tbda:grupoa@vdbase.inesctec.pt:27017/tbda?authSource=admin")
 db = client["tbda"]
 coll = db["districts"]
-coll.insert_many(parse_one_item_arrays(my_dict)['data']['district'])
+try:
+    coll.insert_many(parse_one_item_arrays(my_dict)['data']['districts'])
+except pymongo.errors.BulkWriteError as e:
+    print(e.details)
 
-#print(json.dumps(parse_one_item_arrays(my_dict), indent=4, ensure_ascii=False))
+#print(json.dumps(parse_one_item_arrays(my_dict)['data']['districts'][0], indent=4, ensure_ascii=False))
